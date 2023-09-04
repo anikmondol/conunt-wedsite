ffc876842107b167b5cbb1e5f06d5c539824da4b
anikmondol <anikmondol558363@gmil.com>
2023-09-04 19:12:33 +0600

    first commit

---
+++ js/count.js
@@ -0,0 +1,21 @@
+
+let count = 0
+
+document.getElementById('plus-btn').addEventListener('click',function(){
+    const element = document.getElementById('count');
+
+    count += 1;
+
+    element.innerText = count;
+
+})
+
+
+document.getElementById('minus-btn').addEventListener('click',function(){
+    const element = document.getElementById('count');
+
+    count -= 1;
+
+    element.innerText = count;
+
+})
